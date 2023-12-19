72fde6d5b5d5990b83ad2a2b2f1b2a4a9c4777cf
Orla Collins <ocollin2@terpmail.umd.edu>
2023-12-19 13:23:26 -0500

    Cleaned up the code and made print statements to explain what I scraped from the table

--- final_project.py
+++ final_project.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # This Wikipedia web page contains information about Taylor Swift's albums. It includes a table
-# listing how many albums she's done that fall into different categories
+# listing how many albums she's done that fall into different categories.
 swift_albums_page = requests.get("https://en.wikipedia.org/wiki/Taylor_Swift_albums_discography")
 
 swift_albums_soup = BeautifulSoup(swift_albums_page.content, "html.parser")
@@ -11,7 +11,21 @@ swift_albums_table
 swift_albums_th = swift_albums_table.find_all("th")
 swift_albums_th
 swift_albums_headers = []
-for th in swift_albums_th:
+for th in swift_albums_th[1:]:
     swift_albums_headers.append(th.get_text().strip())
-header_string = ",".join(swift_albums_headers)
-header_string
+print("Here are the types of albums in this table: ")
+swift_albums_headers
+
+table_rows = swift_albums_table.find_all("tr")
+swift_albums_rows = []
+for row in table_rows[2:]:
+    row_list = []
+    print(row)
+    for cell in row.find_all("td"):
+        cell_text = cell.get_text().strip()
+        row_list.append(cell_text)
+    row_string = ",".join(row_list)
+    swift_albums_rows.append(row_string)
+
+print("Here are the corresponding values for the above categories: ")
+swift_albums_rows
