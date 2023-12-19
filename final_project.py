8b3b83c127444dc13a74a16d6307bb1edd889a26
Orla Collins <ocollin2@terpmail.umd.edu>
2023-12-19 12:43:49 -0500

    Changed the web page that I'm scraping, parsed it, found the first table and the headers of it

--- final_project.py
+++ final_project.py
@@ -1,8 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-# This Wikipedia web page contains information about Taylor Swift's Reputation Stadium Tour. I am
-# interested in the Tour dates table which contains data such as Attendance and Revenue for each
-# show.
-reputation_page = requests.get("https://en.wikipedia.org/wiki/Reputation_Stadium_Tour")
+# This Wikipedia web page contains information about Taylor Swift's albums. It includes a table
+# listing how many albums she's done that fall into different categories
+swift_albums_page = requests.get("https://en.wikipedia.org/wiki/Taylor_Swift_albums_discography")
 
+swift_albums_soup = BeautifulSoup(swift_albums_page.content, "html.parser")
+swift_albums_table = swift_albums_soup.find("table")
+swift_albums_table
+swift_albums_th = swift_albums_table.find_all("th")
+swift_albums_th
+swift_albums_headers = []
+for th in swift_albums_th:
+    swift_albums_headers.append(th.get_text().strip())
+header_string = ",".join(swift_albums_headers)
+header_string
